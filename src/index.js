d001b27d54dc5137858bb0658452499f1d4ebedd
code99-ash <oyetikh1@gmail.com>
2021-06-25 18:26:26 +0100

    fresh tutorial on react-redux, facing problem with redux/counter.js

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { createStore } from 'redux';
+import allReducers from './reducers'
+
+const store = createStore(
+  allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+
